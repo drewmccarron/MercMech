d13d8659d050179d57b32914d6d1c38bae68d42e
drewmccarron <59206700+drewmccarron@users.noreply.github.com>
2025-12-28 20:52:41 -0500

    Added playercontrols script

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Actions controls;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        controls.Player.Left.performed += moveLeft;
+        controls.Player.Right.performed += moveRight;
+        controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Left.performed -= moveLeft;
+        controls.Player.Right.performed -= moveRight;
+        controls.Player.Disable();
+    }
+
+    private void moveLeft(InputAction.CallbackContext context)
+    {
+        Debug.Log("Move Left");
+    }
+
+    private void moveRight(InputAction.CallbackContext context)
+    {
+        Debug.Log("Move Right");
+    }
+}
