1a5b31083f3e57f56a25d2486d320652b8c590ac
drewmccarron <59206700+drewmccarron@users.noreply.github.com>
2025-12-28 21:45:28 -0500

    Implement left-right movement

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,8 +4,10 @@ using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb;
     private Actions controls;
-
+    private float moveInput;
+    public float speed = 5f;
 
     void Start()
     {
@@ -15,22 +17,28 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = controls.Player.Walk.ReadValue<float>();
+        moveInput = controls.Player.Walk.ReadValue<float>();
     }
 
     void Awake()
     {
         controls = new Actions();
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     private void OnEnable()
     {
-
+        controls.Player.Enable();
     }
 
     private void OnDisable()
     {
+        controls.Player.Disable();
+    }
 
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
     }
 }
