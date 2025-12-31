3ca1db3d30e4cbad2d61cdec96b0eac05aea8d9c
drewmccarron <59206700+drewmccarron@users.noreply.github.com>
2025-12-31 12:44:23 -0500

    Moved team enum into combatant

--- Assets/Scripts/Combat/Combatant.cs
+++ Assets/Scripts/Combat/Combatant.cs
@@ -6,3 +6,9 @@ public class Combatant : MonoBehaviour
     [SerializeField] private Team team = Team.Enemy;
     public Team Team => team;
 }
+
+public enum Team
+{
+    Player = 0,
+    Enemy = 1,
+}
